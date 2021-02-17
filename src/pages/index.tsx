e12ba50210964b390b5e61e967710036550d1194
keeeei13c <kei.121003f@icloud.com>
2021-02-17 18:42:00 +0900

    add import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NextPage } from 'next'
 import FirstView from '../components/home/first-view'
+import AboutSite from '../components/home/about-site'
+import AboutMe from '../components/home/about-me/about-me'
+import Copyright from '../components/home/copy-right'
 import Layout from '../components/layout'
 
 const Index: NextPage = () => {
   return (
     <Layout title="fukudakei">
       <FirstView />
+      <AboutSite />
+      <AboutMe />
+      <Copyright />
     </Layout>
   );
 };
