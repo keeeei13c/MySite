9403ae2565fc2faaf532d755ef46e2fcc1a05b80
keeeei13c <kei.121003f@icloud.com>
2021-02-24 21:57:06 +0900

    repaired error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,23 @@
 import React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html,Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 import theme from '../components/theme'
 
 export default class MyDocument extends Document {
     render() {
         return (
-            <html lang="ja">
+            <Html>
               <Head>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                    <meta name="theme-color" content={theme.palette.primary.main} />
-                    <script
-            dangerouslySetInnerHTML={{
-              __html:
-                '</script><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,700&display=swap" media="print" onload="this.media=\'all\'" /><script>',
-            }}
-          />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+                {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
+                <meta name="theme-color" content={theme.palette.primary.main} />
               </Head>
             <body>
                     <Main />
                     <NextScript />
             </body>
-            </html>
+          </Html>
+          
         )
     }
 }
